2ca61dd162457d95a52e009b534e0496a52dd479
Rikus Kruger <rikusetiennekruger@gmail.com>
2021-05-20 18:52:55 +0200

    added new model

--- api.py
+++ api.py
@@ -31,7 +31,7 @@ app = Flask(__name__)
 # Please update this path to reflect your own trained model.
 
 static_model = load_model(
-    path_to_model='assets/trained-models/gbr.pkl')
+    path_to_model='assets/trained-models/gbr_model.pkl')
 
 print ('-'*40)
 print ('Model succesfully loaded')
